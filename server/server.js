4518522b108ce23cd3034cd67a3f403b6d23b06f
Toshken21 <anton.rader@gmail.com>
2023-07-19 00:04:20 +0200

    server changes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 process.env.GOOGLE_APPLICATION_CREDENTIALS = "./googleKeyFile.json";
 const cors = require("cors");
 const express = require('express');
-const googleSpeechRoutes = require("./languageRoutes/googleSpeech");
-const gptRoutes = require("./gptRoutes/gptRoutes");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+const googleSpeechRoutes = require("./languageRoutes/googleSpeech");
+const gptRoutes = require("./gptRoutes/gptRoutes");
 
 app.use('/promptVoice', googleSpeechRoutes);
 app.use("/gptPrompt", gptRoutes);
